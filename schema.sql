4239da23c0ab15af31d3f58aa4f9a3f9dd7ec332
Katerine Gomez <kg23736230@IS211-1-1189-13.sps.cuny.edu>
2018-12-21 15:22:25 -0500

    first commit

---
+++ schema.sql
@@ -0,0 +1,59 @@
+CREATE TABLE IF NOT EXISTS AUTHORS (
+
+    author_id  INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    first_name TEXT,
+
+    last_name TEXT,
+
+    ISBN TEXT
+
+);
+
+CREATE TABLE IF NOT EXISTS BOOKS (
+
+    ISBN TEXT PRIMARY KEY,
+
+    title TEXT,
+
+    page_count INTEGER,
+
+    average_rating INTEGER,
+
+    author_name TEXT
+
+);
+
+CREATE TABLE IF NOT EXISTS BOOKS_AUTHORS (
+
+    BA_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    ISBN TEXT,
+
+    author_id INTEGER
+
+);
+
+CREATE TABLE IF NOT EXISTS USERS (
+
+    user_name  TEXT PRIMARY KEY,
+
+    user_first_name TEXT,
+
+    user_last_name TEXT,
+
+    password TEXT    
+
+);
+
+CREATE TABLE IF NOT EXISTS USERS_BOOKS (
+
+    UB_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    user_name TEXT,
+
+    ISBN TEXT,
+
+    Saved_Date TEXT
+
+);
